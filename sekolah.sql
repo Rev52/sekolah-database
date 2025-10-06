10f034065e85ddd47cd5b234c2a9f734566fb90e
Rev52 <revinoakbar52@gmail.com>
2025-10-06 21:57:54 +0700

    Menambahkan table nilai

--- sekolah.sql
+++ sekolah.sql
@@ -20,4 +20,26 @@ INSERT INTO public.siswa (nama,umur,jurusan) VALUES
 	 ('Bradi',16,'IPS'),
 	 ('Rani',16,'IPS');
 
-	 
+-- public.nilai definition
+
+-- Drop table
+
+-- DROP TABLE public.nilai;
+
+CREATE TABLE public.nilai (
+	id serial4 NOT NULL,
+	siswa_id int4 NULL,
+	mata_pelajaran varchar(50) NULL,
+	nilai int4 NULL,
+	CONSTRAINT nilai_pkey PRIMARY KEY (id)
+);
+
+
+-- public.nilai foreign keys
+
+ALTER TABLE public.nilai ADD CONSTRAINT nilai_siswa_id_fkey FOREIGN KEY (siswa_id) REFERENCES public.siswa(id);
+
+INSERT INTO public.nilai (siswa_id,mata_pelajaran,nilai) VALUES
+	 (1,'Fisika',82),
+	 (2,'Bahasa Inggris',90),
+	 (3,'Sejarah',100);
