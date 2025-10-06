83a7ecff912c2e8bfdaa51a44f0e1fc4b4350f2c
Rev52 <revinoakbar52@gmail.com>
2025-10-06 22:23:53 +0700

    Menambahkan query SELECT, UPDATE, dan DELETE

--- sekolah.sql
+++ sekolah.sql
@@ -43,3 +43,35 @@ INSERT INTO public.nilai (siswa_id,mata_pelajaran,nilai) VALUES
 	 (1,'Fisika',82),
 	 (2,'Bahasa Inggris',90),
 	 (3,'Sejarah',100);
+
+-- Tambahan: Query untuk menampilkan siswa yang berjurusan IPA
+SELECT *
+FROM siswa
+WHERE jurusan = 'IPA';
+
+id|nama|umur|jurusan|
+--+----+----+-------+
+ 1|Rere|  16|IPA    |
+
+
+-- Update: Query untuk merubah data jurusan menjadi IPA
+UPDATE siswa
+SET jurusan = 'IPA'
+WHERE id = 2;
+
+id|nama |umur|jurusan|
+--+-----+----+-------+
+ 1|Rere |  16|IPA    |
+ 3|Bradi|  16|IPS    |
+ 2|Rani |  16|IPA    |
+ 
+
+-- Delete: Query untuk menghapus data nilai salah satu siswa
+Delete
+FROM nilai 
+where id = '3';
+
+id|siswa_id|mata_pelajaran|nilai|
+--+--------+--------------+-----+
+ 1|       1|Fisika        |   82|
+ 2|       2|Bahasa Inggris|   90|
